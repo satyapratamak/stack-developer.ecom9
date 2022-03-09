7968ad6f1cae521c47a0b99691b043e4fc94b76f
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2022-03-09 23:27:20 +0700

    Stack Developer Ecom 9 - Video 1 to 8 DONE

---
+++ database/seeders/AdminsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adminRecords = [
+            [
+                'id' => 1,
+                'name' => 'Super Admin',
+                'type' => 'superadmin',
+                'vendor_id' => 0,
+                'mobile' => '980000000',
+                'email' => 'admin@admil.com',
+                'password' => '$2a$12$G6S8xJuqH3Rf6p8g/TC9vugmb6f7XNIF.1wUpBvOg0.yjqsWbI1xy',
+                'image' => '',
+                'status' => 1,
+            ]
+
+        ];
+        Admin::insert($adminRecords);
+    }
+}
